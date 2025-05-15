8350f64fc48a20aea2ca086b018a192f00e1b3eb
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-15 13:31:36 -0500

    Add space as first character test
    I'm making the assumption that "character" in the odin instructions refers to the data type, and not letters.

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -11,3 +11,6 @@ test("Capitalize words", () => {
   });
 });
 test("Empty string", () => expect(capitalize("")).toMatch(""));
+// "A capitalize function that takes a string and returns it with the first character capitalized."
+// Instructions don't specify whether it should be first letter or first character as in the data type, so assume the latter.
+test("Space as first character", () => expect(capitalize(" a")).toMatch(" a"));
