572dec69e0a1adf44221c39a820618d26b64402a
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-15 13:24:58 -0500

    Add empty string test

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -4,3 +4,4 @@ test('Capitalize "apple"', () => expect(capitalize("apple")).toMatch("Apple"));
 test('Capitalize "banana"', () =>
   expect(capitalize("banana")).toMatch("Banana"));
 test('Capitalize "pear"', () => expect(capitalize("pear")).toMatch("Pear"));
+test("Empty string", () => expect(capitalize("")).toMatch(""));
