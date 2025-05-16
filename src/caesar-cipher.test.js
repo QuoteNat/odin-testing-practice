feb8818517648be9f7641a4f245086410329bece
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-16 13:11:33 -0500

    Add wrapping test

--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -3,3 +3,7 @@ import { caesarCipher } from "./caesar-cipher";
 test('"hello world" encrypts to "khoor zruog"', () => {
   expect(caesarCipher("hello world")).toMatch("khoor zruog");
 });
+
+test("Wrapping", () => {
+  expect(caesarCipher("xyz")).toMatch("abc");
+});
