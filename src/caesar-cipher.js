c47f23d96d996d1c2759bc2ee7e1d17b774e90b3
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-16 13:13:20 -0500

    Add shift parameter

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,11 +1,11 @@
 const ALPHABET_LOWER = Array.from("abcdefghijklmnopqrstuvwxyz");
 
-export function caesarCipher(string) {
+export function caesarCipher(string, shift) {
   let stringArray = Array.from(string);
   let encrypted = stringArray.map((char) => {
     let index = ALPHABET_LOWER.indexOf(char);
     if (index == -1) return char;
-    return ALPHABET_LOWER[(index + 3) % 26];
+    return ALPHABET_LOWER[(index + shift) % 26];
   });
   return encrypted.join("");
 }
--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -1,9 +1,9 @@
 import { caesarCipher } from "./caesar-cipher";
 
 test('"hello world" encrypts to "khoor zruog"', () => {
-  expect(caesarCipher("hello world")).toMatch("khoor zruog");
+  expect(caesarCipher("hello world", 3)).toMatch("khoor zruog");
 });
 
 test("Wrapping", () => {
-  expect(caesarCipher("xyz")).toMatch("abc");
+  expect(caesarCipher("xyz", 3)).toMatch("abc");
 });
