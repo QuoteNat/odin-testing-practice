2cdb641a354cc88672963f4922504a716072853e
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-16 13:16:58 -0500

    Add case preservation

--- src/caesar-cipher.js
+++ src/caesar-cipher.js
@@ -1,11 +1,17 @@
 const ALPHABET_LOWER = Array.from("abcdefghijklmnopqrstuvwxyz");
-
+const ALPHABET_UPPER = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 export function caesarCipher(string, shift) {
   let stringArray = Array.from(string);
   let encrypted = stringArray.map((char) => {
-    let index = ALPHABET_LOWER.indexOf(char);
-    if (index == -1) return char;
-    return ALPHABET_LOWER[(index + shift) % 26];
+    if (ALPHABET_LOWER.includes(char)) {
+      let index = ALPHABET_LOWER.indexOf(char);
+      return ALPHABET_LOWER[(index + shift) % 26];
+    } else if (ALPHABET_UPPER.includes(char)) {
+      let index = ALPHABET_UPPER.indexOf(char);
+      return ALPHABET_UPPER[(index + shift) % 26];
+    } else {
+      return char;
+    }
   });
   return encrypted.join("");
 }
--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -7,3 +7,6 @@ test('"hello world" encrypts to "khoor zruog"', () => {
 test("Wrapping", () => {
   expect(caesarCipher("xyz", 3)).toMatch("abc");
 });
+
+test("Case preservation", () =>
+  expect(caesarCipher("HeLLo", 3)).toMatch("KhOOr"));
